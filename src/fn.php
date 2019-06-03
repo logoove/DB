168068ef7caed8f7edc9b006595587b7607f35c6
yoby <logove@qq.com>
2019-06-03 15:18:20 +0800

    2

---
+++ src/fn.php
@@ -0,0 +1,14 @@
+<?php
+/*
+* 文件名: FN.php
+* 作者  : Yoby 微信logove email:logove@qq.com
+* 日期时间: 2019/6/3  14:07
+* 功能  :
+*/
+namespace logoove;
+//include_once __DIR__."/until.php";
+class fn{
+ public static function qq2(){
+     return qq();
+ }
+}
